6468f3083d7bc8e02e1b58bac8190ed5ae8a5564
0xmegalodon <stephen@newbitcoincity.com>
2023-06-16 14:17:03 +0700

    add packages

--- src/modules/Home/SubmitForm/index.tsx
+++ src/modules/Home/SubmitForm/index.tsx
@@ -2,7 +2,7 @@ import Button from "@/components/Button";
 import { Input } from "@/components/Inputs";
 import { validateWalletAddress } from "@/utils";
 import { Formik } from "formik";
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import PaytypeDropdown, { PayType } from "../PaymentForm/PaytypeDropdown";
 import { FormContainer } from "./SubmitForm.styled";
 import Text from "@/components/Text";
@@ -10,8 +10,13 @@ import { ceilPrecised } from "@/utils/format";
 import PackageList from "@/modules/Home/PackageList";
 
 interface IFormValue {
-  amount: string;
+  amountTC: string;
+  amountBTC: string;
+  amountWBTC: string;
   toAddress: string;
+  isCustomPackage: boolean;
+  selectedPackage?: IPackage;
+  payType?: any;
 }
 
 interface SubmitFormProps {
@@ -32,9 +37,9 @@ export interface ICoin {
 }
 
 export interface IPackage {
-  id: number;
-  title: string;
-  coins: ICoin[];
+  id?: number;
+  title?: string;
+  coins?: ICoin[];
 }
 
 const PACKAGES: Array<IPackage> = [
@@ -58,7 +63,7 @@ const PACKAGES: Array<IPackage> = [
       },
       {
         name: 'BTC',
-        amount: 100
+        amount: 0.005
       },
     ]
   },
@@ -72,28 +77,192 @@ const PACKAGES: Array<IPackage> = [
       },
       {
         name: 'BTC',
-        amount: 100
+        amount: 0.005
       },
       {
         name: 'WBTC',
-        amount: 100
+        amount: 0.005
       },
     ]
   },
 ];
 
+const Form = (props: any) => {
+  const {
+    values,
+    errors,
+    touched,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    setFieldValue,
+  } = props;
 
-
-
-const SubmitForm = (props: SubmitFormProps) => {
   const { isProcessing, onSubmitGenerate } = props;
   const [payType, setPayType] = useState(PayType.eth);
   const [isCustomPackage, setIsCustomPackage] = useState(false);
   const [selectedPackage, setSelectedPackage] = useState<IPackage>(PACKAGES[0]);
 
+  useEffect(() => {
+    setFieldValue('isCustomPackage', isCustomPackage, true);
+  }, [isCustomPackage]);
+
+  useEffect(() => {
+    setFieldValue('payType', payType, true);
+  }, [JSON.stringify(payType)]);
+
+  useEffect(() => {
+    if(selectedPackage) {
+      // @ts-ignore
+      setFieldValue('amountTC', selectedPackage?.coins[0]?.amount || 0, true);
+      // @ts-ignore
+      setFieldValue('amountBTC', selectedPackage?.coins[1]?.amount || 0, true);
+      // @ts-ignore
+      setFieldValue('amountWBTC', selectedPackage?.coins[2]?.amount || 0, true);
+
+      setFieldValue('selectedPackage', selectedPackage, true);
+    }
+  }, [JSON.stringify(selectedPackage)]);
+
   const handleSelectPackage = (p: any) => {
     setSelectedPackage(p);
   }
+
+  // @ts-ignore
+  // @ts-ignore
+  return (
+    <FormContainer onSubmit={handleSubmit}>
+      <Input
+        title="Receiving TC Wallet Address"
+        id="toAddress"
+        type="text"
+        name="toAddress"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        value={values.toAddress}
+        className="input"
+        placeholder={`Paste your TC wallet address here (0x1234...2345).`}
+        errorMsg={
+          errors.toAddress && touched.toAddress
+            ? errors.toAddress
+            : undefined
+        }
+      />
+
+      <PaytypeDropdown payType={payType} setPayType={setPayType} />
+      <div>
+        <PackageList data={PACKAGES} onSelect={handleSelectPackage}/>
+        <Text className={"custom-text"} onClick={() => setIsCustomPackage(!isCustomPackage)}>Custom</Text>
+      </div>
+
+      {
+        isCustomPackage && (
+          <div className={"list-inputs"}>
+            <Input
+              // title={`Amount ${
+              //   values.amount && !errors.amount
+              //     ? `( ~${
+              //       payType === PayType.eth
+              //         ? `${ceilPrecised(
+              //           Number(values.amount) * TC_ETH_PRICE,
+              //           4
+              //         )} ETH`
+              //         : `${ceilPrecised(
+              //           Number(values.amount) * TC_BTC_PRICE,
+              //           5
+              //         )} BTC`
+              //     } )`
+              //     : ""
+              // }`}
+              title={"Amount TC"}
+              // id="amount"
+              type="number"
+              name="amountTC"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.amountTC}
+              className="input"
+              placeholder={`Amount TC`}
+              errorMsg={
+                errors.amountTC && touched.amountTC ? errors.amountTC : undefined
+              }
+            />
+            {
+              selectedPackage?.id && selectedPackage?.id > 1 && (
+                <Input
+                  title={"Amount BTC"}
+                  // id="amount"
+                  type="number"
+                  name="amountBTC"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.amountBTC}
+                  className="input"
+                  placeholder={`Amount BTC`}
+                  errorMsg={
+                    errors.amountBTC && touched.amountBTC ? errors.amountBTC : undefined
+                  }
+                />
+              )
+            }
+            {
+              selectedPackage?.id && selectedPackage?.id > 2 && (
+                <Input
+                  title={"Amount WBTC"}
+                  // id="amount"
+                  type="number"
+                  name="amountWBTC"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.amountWBTC}
+                  className="input"
+                  placeholder={`Amount WBTC`}
+                  errorMsg={
+                    errors.amountWBTC && touched.amountWBTC ? errors.amountWBTC : undefined
+                  }
+                />
+              )
+            }
+          </div>
+        )
+      }
+      <Button
+        isLoading={isProcessing}
+        disabled={isProcessing}
+        type="submit"
+        className="confirm-btn"
+      >
+        {isProcessing ? "Processing..." : "Get TC"}
+      </Button>
+
+      <Text className="claimer">
+        <span style={{ fontWeight: "700" }}>Disclaimer:</span> TC is sold
+        only for users to pay gas fees to use the utilities of dapps on TC
+        network (including but not limited to bridging, swapping, creating
+        artifacts, issuing BRC-20 tokens, issuing BRC-721 NFTs, deploying
+        smart contracts, and preserving files), NOT to raise funds. Each
+        wallet is capped at 100 TC. And lastly, US citizens are prohibited
+        from purchasing TC at this time.
+      </Text>
+    </FormContainer>
+  )
+}
+
+const SubmitForm = (props: SubmitFormProps) => {
+  const { isProcessing, onSubmitGenerate } = props;
+  // const [payType, setPayType] = useState(PayType.eth);
+  // const [isCustomPackage, setIsCustomPackage] = useState(false);
+  // const [selectedPackage, setSelectedPackage] = useState<IPackage>(PACKAGES[0]);
+  //
+  // useEffect(() => {
+  //   if(selectedPackage) {
+  //
+  //   }
+  // }, [JSON.stringify(selectedPackage)]);
+  //
+  // const handleSelectPackage = (p: any) => {
+  //   setSelectedPackage(p);
+  // }
 
   const validateForm = (values: IFormValue): Record<string, string> => {
     const errors: Record<string, string> = {};
@@ -106,18 +275,39 @@ const SubmitForm = (props: SubmitFormProps) => {
       errors.toAddress = "Invalid receiving TC wallet address.";
     }
 
-    if (!values.amount) {
-      errors.amount = "Amount is required.";
-    } else if (Number(values.amount) < 0.01 || Number(values.amount) > 100) {
-      errors.amount =
-        "The minimum amount is 0.01 TC. The maximum amount is 100 TC.";
+    if(values?.isCustomPackage) {
+      if (!values.amountTC) {
+        errors.amountTC = "Amount is required.";
+      } else if (Number(values.amountTC) < 0.01 || Number(values.amountTC) > 100) {
+        errors.amountTC =
+          "The minimum amount is 0.01 TC. The maximum amount is 100 TC.";
+      }
+
+      if(values?.selectedPackage?.id && values?.selectedPackage?.id > 1) {
+        if (!values.amountBTC) {
+          errors.amountBTC = "Amount is required.";
+        } else if (Number(values.amountBTC) < 0.001 || Number(values.amountBTC) > 100) {
+          errors.amountBTC =
+            "The minimum amount is 0.001 BTC. The maximum amount is 1 BTC.";
+        }
+      }
+
+      if(values?.selectedPackage?.id && values?.selectedPackage?.id > 2) {
+        if (!values.amountWBTC) {
+          errors.amountWBTC = "Amount is required.";
+        } else if (Number(values.amountWBTC) < 0.001 || Number(values.amountWBTC) > 1) {
+          errors.amountWBTC =
+            "The minimum amount is 0.001 WBTC. The maximum amount is 1 WBTC.";
+        }
+      }
+
     }
 
     return errors;
   };
 
   const handleSubmit = async (payload: IFormValue): Promise<void> => {
-    onSubmitGenerate(payload.toAddress, Number(payload.amount), payType);
+    onSubmitGenerate(payload.toAddress, Number(payload.amountTC), payload.payType);
   };
 
   return (
@@ -125,134 +315,16 @@ const SubmitForm = (props: SubmitFormProps) => {
       key="buy"
       initialValues={{
         toAddress: "",
-        amount: "",
+        amountTC: "",
+        amountBTC: "",
+        amountWBTC: "",
+        isCustomPackage: false
       }}
       validate={validateForm}
       onSubmit={handleSubmit}
     >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-      }) => (
-        <FormContainer onSubmit={handleSubmit}>
-          <Input
-            title="Receiving TC Wallet Address"
-            id="toAddress"
-            type="text"
-            name="toAddress"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.toAddress}
-            className="input"
-            placeholder={`Paste your TC wallet address here (0x1234...2345).`}
-            errorMsg={
-              errors.toAddress && touched.toAddress
-                ? errors.toAddress
-                : undefined
-            }
-          />
-
-
-          <PaytypeDropdown payType={payType} setPayType={setPayType} />
-          <div>
-            <PackageList data={PACKAGES} onSelect={handleSelectPackage}/>
-            <Text className={"custom-text"} onClick={() => setIsCustomPackage(!isCustomPackage)}>Custom</Text>
-          </div>
-
-          {
-            isCustomPackage && (
-              <div className={"list-inputs"}>
-                <Input
-                  // title={`Amount ${
-                  //   values.amount && !errors.amount
-                  //     ? `( ~${
-                  //       payType === PayType.eth
-                  //         ? `${ceilPrecised(
-                  //           Number(values.amount) * TC_ETH_PRICE,
-                  //           4
-                  //         )} ETH`
-                  //         : `${ceilPrecised(
-                  //           Number(values.amount) * TC_BTC_PRICE,
-                  //           5
-                  //         )} BTC`
-                  //     } )`
-                  //     : ""
-                  // }`}
-                  title={"Amount TC"}
-                  // id="amount"
-                  type="number"
-                  name="amountTC"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.amount}
-                  className="input"
-                  placeholder={`Amount TC`}
-                  errorMsg={
-                    errors.amount && touched.amount ? errors.amount : undefined
-                  }
-                />
-                {
-                  selectedPackage?.id > 1 && (
-                    <Input
-                      title={"Amount BTC"}
-                      // id="amount"
-                      type="number"
-                      name="amountBTC"
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.amount}
-                      className="input"
-                      placeholder={`Amount BTC`}
-                      errorMsg={
-                        errors.amount && touched.amount ? errors.amount : undefined
-                      }
-                    />
-                  )
-                }
-                {
-                  selectedPackage?.id > 2 && (
-                    <Input
-                      title={"Amount WBTC"}
-                      // id="amount"
-                      type="number"
-                      name="amountWBTC"
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.amount}
-                      className="input"
-                      placeholder={`Amount WBTC`}
-                      errorMsg={
-                        errors.amount && touched.amount ? errors.amount : undefined
-                      }
-                    />
-                  )
-                }
-              </div>
-            )
-          }
-          <Button
-            isLoading={isProcessing}
-            disabled={isProcessing}
-            type="submit"
-            className="confirm-btn"
-          >
-            {isProcessing ? "Processing..." : "Get TC"}
-          </Button>
-
-          <Text className="claimer">
-            <span style={{ fontWeight: "700" }}>Disclaimer:</span> TC is sold
-            only for users to pay gas fees to use the utilities of dapps on TC
-            network (including but not limited to bridging, swapping, creating
-            artifacts, issuing BRC-20 tokens, issuing BRC-721 NFTs, deploying
-            smart contracts, and preserving files), NOT to raise funds. Each
-            wallet is capped at 100 TC. And lastly, US citizens are prohibited
-            from purchasing TC at this time.
-          </Text>
-        </FormContainer>
+      {(props) => (
+        <Form {...props}/>
       )}
     </Formik>
   );
